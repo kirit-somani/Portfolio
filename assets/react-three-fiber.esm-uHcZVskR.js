86de442426d2cfb52e2ce8d0293b2842a4d973a0
kirit-somani <somani2004kanav@gmail.com>
2026-07-28 22:54:15 +0530

    Updates

--- assets/react-three-fiber.esm-uHcZVskR.js
+++ assets/react-three-fiber.esm-uHcZVskR.js
@@ -1,4 +1,4 @@
-import{r as Xe,g as gx,j as In}from"./index-BEUDAN5Y.js";/**
+import{r as Xe,g as gx,j as In}from"./index-Cgg0mABW.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
